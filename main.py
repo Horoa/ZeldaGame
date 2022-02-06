b3908d39166e8565bbae8d6785e5e9e2a6ba4cbe
hode <hoderomain@eisti.eu>
2022-02-06 21:48:05 +0100

    Add level, main, player, tiles

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from settings import *
+from level import *
 
 
 class Game:
@@ -8,7 +9,10 @@ class Game:
         #general setup
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGTH))
+        pygame.display.set_caption('Turbo Hunter Tactics')
         self.clock = pygame.time.Clock()
+
+        self.level = Level()
 
     def run(self):
         while True:
@@ -17,6 +21,7 @@ class Game:
                     pygame.quit()
                     sys.exit()
             self.screen.fill('black')
+            self.level.run()
             pygame.display.update()
             self.clock.tick(FPS)
 
