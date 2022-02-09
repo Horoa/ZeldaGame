199cd66d3760d7990211fec55d492c0c2b77762f
Horoa <hoderomain64@gmail.com>
2022-02-09 01:40:19 +0100

    Adding enemy interaction + grass interaction with player.py

--- level.py
+++ level.py
@@ -20,6 +20,8 @@ class Level:
 
         # attack sprites
         self.current_attack = None
+        self.attack_sprites = pygame.sprite.Group()
+        self.attackable_sprites = pygame.sprite.Group()
 
         # sprite setup
         self.create_map()
@@ -49,7 +51,11 @@ class Level:
                         if style == 'grass':
                             # create grass tile
                             random_grass_image = choice(graphics['grass'])
-                            Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'grass', random_grass_image)
+                            Tile(
+                                (x, y),
+                                [self.visible_sprites, self.obstacle_sprites, self.attackable_sprites],
+                                'grass',
+                                random_grass_image)
                         if style == 'object':
                             surface = graphics['objects'][int(col)]
                             Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'object', surface)
@@ -73,10 +79,15 @@ class Level:
                                 else:
                                     monster_name = 'squid'
 
-                                Enemy(monster_name, (x, y), [self.visible_sprites], self.obstacle_sprites)
+                                Enemy(
+                                    monster_name,
+                                    (x, y),
+                                    [self.visible_sprites, self.attackable_sprites],
+                                    self.obstacle_sprites,
+                                    self.damage_player)
 
     def create_attack(self):
-        self.current_attack = Weapon(self.player, [self.visible_sprites])
+        self.current_attack = Weapon(self.player, [self.visible_sprites, self.attack_sprites])
 
     def create_magic(self, style, strength, cost):
         print(style)
@@ -91,11 +102,30 @@ class Level:
     def destroy_magic(self):
         pass
 
+    def player_attack_logic(self):
+        if self.attack_sprites:
+            for attack_sprite in self.attack_sprites:
+                collision_sprites = pygame.sprite.spritecollide(attack_sprite, self.attackable_sprites, False)
+                if collision_sprites:
+                    for target_sprite in collision_sprites:
+                        if target_sprite.sprite_type == 'grass':
+                            target_sprite.kill()
+                        elif target_sprite.sprite_type == 'enemy':
+                            target_sprite.get_damage(self.player, attack_sprite.sprite_type)
+
+    def damage_player(self, amount, attack_type):
+        if self.player.vulnerable:
+            self.player.health -= amount
+            self.player.vulnerable = False
+            self.player.hurt_time = pygame.time.get_ticks()
+            # spawn particles
+
     def run(self):
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
         self.visible_sprites.enemy_update(self.player)
+        self.player_attack_logic()
         self.ui.display(self.player)
 
 
@@ -127,7 +157,6 @@ class YSortCameraGroup(pygame.sprite.Group):
             self.display_surface.blit(sprite.image, offset_position)
 
     def enemy_update(self, player):
-        pass
-        # enemy_sprites = [sprite for sprite in self.sprites() if sprite.sprite_type == 'enemy']
-        # for enemy in enemy_sprites:
-        #     sprite.enemy_update(player) # 3h49min30
+        enemy_sprites = [sprite for sprite in self.sprites() if hasattr(sprite, 'sprite_type') and sprite.sprite_type == 'enemy']
+        for enemy in enemy_sprites:
+            enemy.enemy_update(player)  # 3h49min30
--- weapon.py
+++ weapon.py
@@ -4,6 +4,7 @@ import pygame
 class Weapon(pygame.sprite.Sprite):
     def __init__(self, player, groups):
         super().__init__(groups)
+        self.sprite_type = 'weapon'
         orientation = player.orientation
 
         # graphic
